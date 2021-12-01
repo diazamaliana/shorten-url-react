cad1ffeb1da176f9ca0de3b097d69b09af8e2840
Diaz Amaliana <diazamaliana@gmail.com>
2021-12-01 23:37:52 +0700

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Footer from 'components/Footer';
+import Header from 'components/Header';
+import LandingPage from 'pages/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <LandingPage></LandingPage>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
