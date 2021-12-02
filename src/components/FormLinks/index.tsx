aff73cf52c64169a3786eabaa33611104c94b988
Diaz Amaliana <diazamaliana@gmail.com>
2021-12-02 21:56:24 +0700

    feat: add  hooks to components

--- src/components/FormLinks/index.tsx
+++ src/components/FormLinks/index.tsx
@@ -1,23 +1,55 @@
+import React, { useState } from 'react';
 import Button from 'components/Button';
 import './styles.scss';
 
-const FormsLinks = () => {
+const FormsLinks = ({ onUrlSubmit, ...rest }: any) => {
+  const [url, setUrl] = useState('');
+  const [formError, setFormError] = useState('');
+  const [isValid, setIsValid] = React.useState<boolean>(true);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!url || !url.trim()) {
+      setFormError('Please add a link');
+      setIsValid(false);
+    } else {
+      setFormError('');
+      setIsValid(true);
+      onUrlSubmit(url);
+    }
+
+    setUrl('');
+  };
+
   return (
     <div className="form">
-      <form className="form__input">
-        <input
-          type="text"
-          className="form__input-field"
-          placeholder={'Shorten a link here...'}
-        />
-        <Button
-          buttonStyle="btn--secondary"
-          buttonSize="btn--medium"
-          type="submit"
-        >
-          {'Shorten it!'}
-        </Button>
-      </form>
+      <div className="form__box">
+        <form className="form__box-input" onSubmit={handleSubmit}>
+          <input
+            {...rest}
+            type="text"
+            className={
+              isValid
+                ? 'form__box-input--field'
+                : 'form__box-input--field-alert'
+            }
+            placeholder={'Shorten a link here...'}
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <Button
+            buttonStyle="btn--secondary"
+            buttonSize="btn--medium"
+            type="submit"
+          >
+            {'Shorten it!'}
+          </Button>
+        </form>
+        {formError ? (
+          <p className="form__box-input--alert">{formError}</p>
+        ) : null}
+      </div>
       <div className="form__link">
         <p className="form__link-original">Hello</p>
         <a className="form__link-shorten" href="">
--- src/components/Stats/index.tsx
+++ src/components/Stats/index.tsx
@@ -2,11 +2,16 @@ import './styles.scss';
 import Card from 'components/Card';
 import { CARDS, content } from './content';
 import FormLinks from 'components/FormLinks';
+import useShorten from 'hooks/useShorten';
 
 const Stats = () => {
+  const { appError, isLoading, getShortenUrl } = useShorten();
+
   return (
     <section className="statistic">
-      <FormLinks></FormLinks>
+      <FormLinks onUrlSubmit={getShortenUrl}></FormLinks>
+      {appError ? <p>Error</p> : null}
+      {isLoading ? <p>Loading...</p> : null}
       <div className="statistic__content">
         <h2 className="statistic__content-title">{content.title}</h2>
         <p className="statistic__content-text">{content.text}</p>
