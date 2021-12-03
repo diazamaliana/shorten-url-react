afbcea8971e12cba29a0bfc42550386fccd03742
Diaz Amaliana <diazamaliana@gmail.com>
2021-12-03 21:51:04 +0700

    feat: add loader components

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,14 @@
+import './styles.scss';
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="loader__dot"></div>
+      <div className="loader__dot loader__dot--1"></div>
+      <div className="loader__dot loader__dot--2"></div>
+      <div className="loader__dot loader__dot--3"></div>
+    </div>
+  );
+};
+
+export default Loader;
